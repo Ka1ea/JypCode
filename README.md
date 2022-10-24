aeaa5f5ccdb1ea4cc68e96ea2e85151440d0a0ef
Kalea Gin <gink@purdue.edu>
2022-10-24 00:01:02 -0400

    Merged template with code
    Merged template with code, added link to github and some visual adjustments.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # JypCode
-Jypyter notebook code extraction :)
+### Jypyter notebook code extraction :)
+
+---
+
+Author: Kalea Gin
+Last Updated: 10/23/2022
+Version: 1.0
+
+---
+
+Extracts the code from the cells of a Jypyter Notebook and returns a code file of just the code. 
+
+Alternate Python script in src if the website isn't working for any reason.
+
+Site template credit: https://codepen.io/roydigerhund/pen/ZQdbeN 
--- assets/index.js
+++ assets/index.js
@@ -1,58 +1,110 @@
-const output = document.getElementById('output');
-
-function getMetadataForFileList(fileList) {
-  for (const file of fileList) {
-    // Not supported in Safari for iOS.
-    const name = file.name ? file.name : 'NOT SUPPORTED';
-    // Not supported in Firefox for Android or Opera for Android.
-    const type = file.type ? file.type : 'NOT SUPPORTED';
-    // Unknown cross-browser support.
-    const size = file.size ? file.size : 'NOT SUPPORTED';
-    console.log({
-      file,
-      name,
-      type,
-      size
-    });
-  }
-}
+$(document).ready(function () {
 
 
-function getSrc(block) {
-  if (block.cell_type == "code") {
-    block.source.forEach((code) => {
-      output.innerText = "\n" + code + output.innerText;
+    var droppedFiles = false;
+    var fileName = '';
+    var $dropzone = $('.dropzone');
+    var $button = $('.upload-btn');
+    var uploading = false;
+    var $syncing = $('.syncing');
+    var $done = $('.done');
+    var $bar = $('.bar');
+    var timeOut;
+
+    $dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
     })
-  }
-}
+        .on('dragover dragenter', function () {
+            $dropzone.addClass('is-dragover');
+        })
+        .on('dragleave dragend drop', function () {
+            $dropzone.removeClass('is-dragover');
+        })
+        .on('drop', function (e) {
+            droppedFiles = e.originalEvent.dataTransfer.files;
+            fileName = droppedFiles[0]['name'];
+            $('.filename').html(fileName);
+            $('.dropzone .upload').hide();
+        });
 
 
-
-const cStatus = document.getElementById('status');
-if (window.FileList && window.File && window.FileReader) {
-  document.getElementById('file-selector').addEventListener('change', event => {
-    output.src = '';
-    cStatus.textContent = '';
-    const file = event.target.files[0];
-    
-		if (!file.name) {
-      cStatus.textContent = 'Error: The File.name property does not appear to be supported on this browser.';
-      return;
-    }
-    if (!file.name.match('.*\.ipynb')) {
-      cStatus.textContent = 'Error: The selected file does not appear to be an JypyterNotebook.'
-      return;
-    }
-
-    const reader = new FileReader();
-    reader.addEventListener('load', event => {
-      const json = event.target.result;
-      const obj = JSON.parse(json)
-      obj.cells.reverse().forEach(getSrc);
+    $("input:file").change(function () {
+        fileName = $(this)[0].files[0].name;
+        $('.filename').html(fileName);
+        $('.dropzone .upload').hide();
     });
-    reader.readAsText(file);
-    getMetadataForFileList(event.target.files)
-  });
-} else {
-    cStatus.innerText = "Error browser incompatible with application";
-}
+
+    function startUpload() {
+        if (!uploading && fileName != '') {
+            uploading = true;
+            $button.html('Uploading...');
+            $dropzone.fadeOut();
+            $syncing.addClass('active');
+            $done.addClass('active');
+            $bar.addClass('active');    
+            timeoutID = window.setTimeout(showDone, 1000);
+        }
+    }
+
+    function showDone() {
+
+        $button.html('Done');
+    }
+
+
+
+    function getSrc(block) {
+        if (block.cell_type == "code") {
+            block.source.forEach((code) => {
+                document.getElementById("temp").textContent += "\n" + code;
+                console.log("\n" + code);
+            })
+        }
+    }
+
+
+
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+
+
+
+    if (window.FileList && window.File && window.FileReader) {
+        const selector = document.getElementById('submit-button').addEventListener('click', () => {
+            startUpload();
+            const file = document.getElementById('file-selector').files[0];
+
+            if (!file.name) {
+                alert('Error: The File.name property does not appear to be supported on this browser.');
+                return;
+            }
+            if (!file.name.match('.*\.ipynb')) {
+                alert('Error: The selected file does not appear to be an JypyterNotebook.');
+                return;
+            }
+
+            const reader = new FileReader();
+            reader.addEventListener('load', event => {
+
+                const json = event.target.result;
+                const obj = JSON.parse(json);
+                var allCode = obj.cells.forEach(getSrc);
+                var codeType = file.name.substring(0, file.name.length - 6) + obj.metadata.language_info.file_extension.toString()
+                download(codeType, document.getElementById("temp").textContent);
+                document.getElementById("temp").textContent = "";
+            });
+            reader.readAsText(file);
+        });
+    } 
+});
